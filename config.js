ab2792eb632ba125736b0e1810cd2425ac3bfad3
Eric Johnson <ejohnson@showpitch.com>
2015-04-02 20:02:34 -0600

    Cleaned out unneded modules and updated gitignore

--- config.js
+++ config.js
@@ -13,16 +13,3 @@ System.config({
   }
 });
 
-System.config({
-  "map": {
-    "babel": "npm:babel@4.7.16",
-    "babel-runtime": "npm:babel-runtime@4.7.16",
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "npm:babel-runtime@4.7.16": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    }
-  }
-});
-
